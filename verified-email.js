9bea9dd83b766e821098ab480f0510ff29c86b46
= <seankeane93@gmail.com>
2024-02-16 21:38:21 -0800

    Updated files to meet requirments

--- verified-email.js
+++ verified-email.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const oregonForm = document.getElementById('contactForm');
+  const oregonForm = document.getElementById('Contact-Form');
   oregonForm.addEventListener('submit', function(event) {
     const enteredEmail = document.getElementById('email').value;
     const confirmEmail = document.getElementById('confirmEmail').value;
